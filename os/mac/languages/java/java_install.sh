c2b1e11524d05b8ad3967144334d4cabdc43a7b3
Andrew Dabrowski <andrewdabrowski@Andrews-MacBook-Air.local>
2017-10-30 20:49:08 -0400

    added java and intellij community edition installs

---
+++ os/mac/languages/java/java_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Purpose: Install latest java
+brew cask install java
---
+++ os/mac/tools/intellij_ce_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Purpose: Install community edition of intellij idea community edition
+brew cask install intellij-idea-ce
